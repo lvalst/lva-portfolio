1b7ecbe89ed909b2dd2578f7b2d614ee0d6ddac1
Lauren Van Allen <lvalstine@gmail.com>
2019-12-26 14:56:15 -0600

    more of those responsive things

---
+++ main.js
@@ -0,0 +1,20 @@
+
+// formatted ramble//
+<pre><code class="ramble">
+The very second you find that single
+    misplaced piece of the puzzle
+        and it was so freaking simple,
+            and you want to—<em>need</em> to—, 
+                scream and throw something—!
+But then you find
+    your answer spawned interesting, <em>new</em>, problems
+        and you’re enthralled again and ignoring 
+            the aching of your shoulders,
+                    the itching of your eyes,
+                        the growl of your stomach.
+    The rest of the world fades to nothing,
+        leaving only your
+            monitor,
+                keyboard,
+                    fingers...
+        ...just me?</code></pre>
